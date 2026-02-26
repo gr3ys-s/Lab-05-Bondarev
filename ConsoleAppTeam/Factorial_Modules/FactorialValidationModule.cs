da864a8066e8991dfea2c2ec7a2c36b1dd430882
Sam-K-V <ksemen901@gmail.com>
2026-02-26 20:14:01 +0700

    Добавлен новый модуль для 3 варианта (обрабтка введенных данных)

---
+++ ConsoleAppTeam/Factorial_Modules/FactorialValidationModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleAppTeam.Factorial_Modules
+{
+    public class FactorialValidationModule
+    {
+        /// <summary>
+        /// Проверка на коррекность числа (если оно < 0, то факториал нельзя определить)
+        /// </summary>
+        /// <param name="number">число для расчета факториала</param>
+        /// <returns></returns>
+        public static bool CheckNumber(int number)
+        {
+            return number >= 0;
+        }
+    }
+}
